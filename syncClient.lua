3411076e09ce0f67d36a39c92829a08ae5354a19
Nathan <nathan@silverchip.com>
2021-07-27 01:42:32 +0100

    changed order

--- syncClient.lua
+++ syncClient.lua
@@ -1,15 +1,15 @@
 local ARGS = { ... };
 
-local REPO_FULL = ARGS[1];
-local GITHUB_ACCESS_TOKEN = ARGS[2];
-local PROGRAM = ARGS[3];
-local PROGRAM_ARGS = ARGS[4];
-local DIR = ARGS[5];
-local PORT = ARGS[6];
+local CHANNEL = ARGS[1];
+local REPO_FULL = ARGS[2];
+local GITHUB_ACCESS_TOKEN = ARGS[3];
+local DIR = ARGS[4];
+local PROGRAM = ARGS[5];
+local PROGRAM_ARGS = ARGS[6];
 
+print(CHANNEL)
 print(REPO_FULL)
 print(GITHUB_ACCESS_TOKEN)
+print(DIR)
 print(PROGRAM)
 print(PROGRAM_ARGS)
-print(DIR)
-print(PORT)
