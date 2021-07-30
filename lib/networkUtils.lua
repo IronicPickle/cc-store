20128d6637e1e0413e4e4fd25136cdc00d9e34f8
Nathan <nathan@silverchip.com>
2021-07-30 19:25:11 +0100

    added new network setup

---
+++ lib/networkUtils.lua
@@ -0,0 +1,79 @@
+-- Exported table
+local M = {}
+
+
+function M.joinOrCreate(channel, device, onChange)
+  local modem = peripheral.find("modem")
+  local devices = {}
+  local isHost = false
+
+  local function startListener()
+    while true do
+      local event, _, _, _, body = os.pullEvent()
+
+      if event == "modem_message" then
+        if isHost then
+          if body.type == "/network/join" then
+            table.insert(devices, body.device)
+            modem.transmit(channel, channel, {
+              type = "network/update",
+              devices = devices
+            })
+            onChange(devices)
+          end
+        else
+          if body.type == "/network/update" then
+            devices = body.devices
+            onChange(devices)
+          end
+        end
+      end
+
+    end
+  end
+  
+  local function attemptJoinNetwork()
+
+    local function join()
+      print(" > Attempting to join network on "..channel)
+      modem.transmit(channel, channel,
+        {
+              type = "/network/join",
+              device = device
+        }
+      )
+
+      while true do
+        local event, _, _, _, body = os.pullEvent()
+        if event == "modem_message" then
+          if body.type == "network/join-res" then
+            devices = body.devices
+            onChange(devices)
+          end
+        end
+      end
+    end
+
+    parallel.waitForAny(join,
+      function()
+        print(" > No network fonund, assuming host... ")
+        os.sleep(5)
+        isHost = true
+      end
+    )
+
+  end
+
+  modem.open(channel)
+
+  attemptJoinNetwork()
+  if isHost then
+    devices = { device }
+    onChange(devices)
+  end
+
+  startListener()
+
+  return isHost
+
+end
