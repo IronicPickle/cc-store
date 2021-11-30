64cb4cff89d7e1151f913a6f0bd4f669d1916e25
Nathan Yakkr <nathan.rath@yakkr.co.uk>
2021-11-30 22:38:06 +0000

    added start of quarry program

---
+++ programs/quarry.lua
@@ -0,0 +1,16 @@
+--$ARGS|Channel (40)|$ARGS
+
+local turtleUtils = require("/lua/lib/turtleUtils")
+local TrackedTurtle = turtleUtils.TrackedTurtle
+
+-- Args
+
+-- Main
+function start()
+  print("# Program Started")
+
+  local start = {10, 10, 10}
+
+end
+
+start()
