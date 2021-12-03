f39e988bc8c3904b114b7ee38e2c3f6b53e142a3
Nathan Yakkr <nathan.rath@yakkr.co.uk>
2021-12-03 22:50:05 +0000

    quarry program complete

--- lib/WrappedTurtle.lua
+++ lib/WrappedTurtle.lua
@@ -17,7 +17,17 @@ function WrappedTurtle:new(turtle)
   local o = { turtle = turtle, orientation = 0, x = 0, y = 0, z = 0, mode = "nominal" }
   setmetatable(o, self)
   self.__index = self
+
   return o
+end
+
+function WrappedTurtle:resume()
+  self:loadState()
+  local mode = self.mode or "nominal"
+
+  if mode == "returning" then self:returnToTracker()
+  elseif mode == "depositing inventory" then self:depositInventory()
+  elseif mode == "taking fuel" then self:takeFuel() end
 end
 
 function WrappedTurtle:resetTracker()
@@ -25,91 +35,98 @@ function WrappedTurtle:resetTracker()
   self.x = 0
   self.y = 0
   self.z = 0
+  self:saveState()
 end
 
-function WrappedTurtle:forward(distance, dig)
+function WrappedTurtle:forward(distance, dig, bypassFuel)
   distance = distance or 1
-  local travelled = 0
   for i = 1, distance do
-    if dig then self:dig() end
-    if self:refuel() and self.turtle.forward() then
+    if self:canForward(dig, bypassFuel) then
+      if dig then self:dig() end
       self:updateTrackerCoords(1)
-      travelled = travelled + 1
-    else break end
-  end
-  return travelled == distance, travelled
-end
-
-function WrappedTurtle:back(distance)
-  distance = distance or 1
-  local travelled = 0
-  for i = 1, distance do
-    if self:refuel() and self.turtle.back() then
-      self:updateTrackerCoords(-1)
-      travelled = travelled + 1
+      self.turtle.forward()
     end
   end
-  return travelled == distance, travelled
 end
 
-function WrappedTurtle:right(distance, dig)
+function WrappedTurtle:back(distance, bypassFuel)
+  distance = distance or 1
+  for i = 1, distance do
+    if (self:refuel() or bypassFuel) and self.turtle.back() then
+      self:updateTrackerCoords(-1)
+    end
+  end
+end
+
+function WrappedTurtle:right(distance, dig, bypassFuel)
   distance = distance or 1
   self:turnRight()
-  return self:forward(distance, dig)
+  return self:forward(distance, dig, bypassFuel)
 end
 
-function WrappedTurtle:left(distance, dig)
+function WrappedTurtle:left(distance, dig, bypassFuel)
   distance = distance or 1
   self:turnLeft()
-  return self:forward(distance, dig)
+  return self:forward(distance, dig, bypassFuel)
 end
 
-function WrappedTurtle:up(distance, dig)
+function WrappedTurtle:up(distance, dig, bypassFuel)
   distance = distance or 1
-  local travelled = 0
   for i = 1, distance do
-    if dig then self:digUp() end
-    if self:refuel() and self.turtle.up() then
+    if self:canUp(dig, bypassFuel) then
+      if dig then self:digUp() end
       self:updateTrackerCoords(1, 4)
-      travelled = travelled + 1
-    else break end
+      self.turtle.up()
+    end
   end
-  return travelled == distance, travelled
 end
 
-function WrappedTurtle:down(distance, dig)
+function WrappedTurtle:down(distance, dig, bypassFuel)
   distance = distance or 1
-  local travelled = 0
   for i = 1, distance do
-    if dig then self:digDown() end
-    if self:refuel() and self.turtle.down() then
+    if self:canDown(dig, bypassFuel) then
+      if dig then self:digDown() end
       self:updateTrackerCoords(1, 5)
-      travelled = travelled + 1
-    else break end
+      self.turtle.down()
+    end
   end
-  return travelled == distance, travelled
+end
+
+function WrappedTurtle:canForward(dig, bypassFuel)
+  if dig and bypassFuel then return true
+  elseif dig then return self:refuel()
+  else return self:refuel() and not self:detect() end
+end
+
+function WrappedTurtle:canDown(dig, bypassFuel)
+  if dig and bypassFuel then return true
+  elseif dig then return self:refuel()
+  else return self:refuel() and not self:detectDown() end
+end
+
+function WrappedTurtle:canUp(dig, bypassFuel)
+  if dig and bypassFuel then return true
+  elseif dig then return self:refuel()
+  else return self:refuel() and not self:detectUp() end
 end
 
 function WrappedTurtle:turnRight()
-  local success = self.turtle.turnRight()
-  if not success then return false end
   local newOrientation = self.orientation + 1
   if newOrientation > 3 then newOrientation = 0 end
-  self.orientation = newOrientation
-  return true
+  self:updateTrackerOrientation(newOrientation)
+  self.turtle.turnRight()
 end
 
 function WrappedTurtle:turnLeft()
-  local success = self.turtle.turnLeft()
-  if not success then return false end
   local newOrientation = self.orientation - 1
   if newOrientation < 0 then newOrientation = 3 end
-  self.orientation = newOrientation
-  return true
+  self:updateTrackerOrientation(newOrientation)
+  self.turtle.turnLeft()
 end
 
 function WrappedTurtle:turnAround()
-  return self:turnLeft() and self:turnLeft()
+  self:turnLeft()
+  self:turnLeft()
 end
 
 function WrappedTurtle:move(direction)
@@ -156,25 +173,49 @@ function WrappedTurtle:digDown()
   end
 end
 
+function WrappedTurtle:detect()
+  return self.turtle.detect()
+end
+
+function WrappedTurtle:detectUp()
+  return self.turtle.detectUp()
+end
+
+function WrappedTurtle:detectDown()
+  return self.turtle.detectDown()
+end
+
+function WrappedTurtle:inspect()
+  return self.turtle.inspect()
+end
+
+function WrappedTurtle:inspectUp()
+  return self.turtle.inspectUp()
+end
+
+function WrappedTurtle:inspectDown()
+  return self.turtle.inspectDown()
+end
+
 function WrappedTurtle:getFuelLevel()
   local level = self.turtle.getFuelLevel()
   if level == "unlimited" then level = math.huge end
   return level, level <= 0
 end
 
+function WrappedTurtle:needsFuel(offset)
+  offset = offset or 0
+  local blocksToTracker = math.abs(self.x) + math.abs(self.y) + math.abs(self.z)
+  return self:getFuelLevel() < (blocksToTracker + 100 + offset)
+end
+
 function WrappedTurtle:refuel()
   local prevSlot = self:getSelectedSlot()
-  local blocksToTracker = math.abs(self.x) + math.abs(self.y) + math.abs(self.z)
-  self:select(16)
-  print("blocks to tracker: " .. blocksToTracker)
-  print("fuel level: " .. self:getFuelLevel())
   local attempts = 0
-  while self:getFuelLevel() < (blocksToTracker + 100) do
+  while self:needsFuel(-1) do
     attempts = attempts + 1
-    if not self.turtle.refuel(1) then
-      if attempts == 1 then print("refuel failed... waiting...") end
-      sleep(5)
-    else print("refueled") end
+    self:select(16)
+    if not self.turtle.refuel(1) then return false end
   end
   self:select(prevSlot)
   return true
@@ -193,37 +234,56 @@ end
 
 function WrappedTurtle:returnToTracker(dig, order)
   order = order or {"z", "x", "y"}
-  self.mode = "returning"
-  local x, y, z = self.x, self.y, self.z
-  self:reorient()
+  local x, y, z, orientation = self.x, self.y, self.z, self.orientation
+
+  if self.mode == "returning" then
+    x, y, z = (self.targetX or 0) + x, (self.targetY or 0) + y, (self.targetZ or 0) + z
+    orientation = (self.targetOrientation or 0) or orientation
+    order = self.targetOrder or order
+  end
+
+  self.targetX = x
+  self.targetY = y
+  self.targetZ = z
+  self.targetOrientation = orientation
+  self.targetOrder = order
+  self:setMode("returning")
+
+  function calcOrientation()
+    local o = self.orientation + orientation
+    local r = o % 3
+    if r > 0 then o = r end
+    return o
+  end
+
   self:resetTracker()
 
   local actions = {
     x = function()
-      self:reorient()
+      self:reorient(calcOrientation())
       if x > 0 then
-        self:turnAround()
-        self:forward(math.abs(x), dig)
+        self:left(math.abs(x), dig, true)
       else
-        self:forward(math.abs(x), dig)
+        self:right(math.abs(x), dig, true)
       end
-      self:reorient()
-    end,
-    z = function()
-      self:reorient()
-      if z > 0 then
-        self:left(math.abs(z), dig)
-      else
-        self:right(math.abs(z), dig)
-      end
-      self:reorient()
+      self:reorient(calcOrientation())
     end,
     y = function()
       if y > 0 then
-        self:down(math.abs(y), dig)
+        self:down(math.abs(y), dig, true)
       else
-        self:up(math.abs(y), dig)
+        self:up(math.abs(y), dig, true)
       end
+    end,
+    z = function()
+      self:reorient(calcOrientation())
+      if z > 0 then
+        self:turnAround()
+        self:forward(math.abs(z), dig, true)
+      else
+        self:forward(math.abs(z), dig, true)
+      end
+      self:reorient(calcOrientation())
     end
   }
 
@@ -231,8 +291,13 @@ function WrappedTurtle:returnToTracker(dig, order)
     actions[order[i]]()
   end
 
-  self:reorient()
-  self.mode = "nominal"
+  self.targetX = nil
+  self.targetY = nil
+  self.targetZ = nil
+
+  self:reorient(calcOrientation())
+  self:saveState()
+  self:setMode("nominal")
 end
 
 function WrappedTurtle:select(slot)
@@ -271,7 +336,7 @@ end
 
 function WrappedTurtle:drop(slot, amount)
   amount = amount or 1
-  self:interact("foward", slot, "drop", amount)
+  self:interact("forward", slot, "drop", amount)
 end
 
 function WrappedTurtle:dropUp(slot, amount)
@@ -286,7 +351,7 @@ end
 
 function WrappedTurtle:suck(slot, amount)
   amount = amount or 1
-  self:interact("foward", slot, "suck", amount)
+  self:interact("forward", slot, "suck", amount)
 end
 
 function WrappedTurtle:suckUp(slot, amount)
@@ -299,18 +364,24 @@ function WrappedTurtle:suckDown(slot, amount)
   self:interact("down", slot, "suck", amount)
 end
 
+function WrappedTurtle:updateTrackerOrientation(orientation)
+  self.orientation = orientation
+  self:saveState()
+end
+
 function WrappedTurtle:updateTrackerCoords(distance, orientation)
   if orientation == nil then orientation = self.orientation end
   local actions = {
-    [0] = function() self.x = self.x + distance end,
-    [1] = function() self.z = self.z + distance end,
-    [2] = function() self.x = self.x - distance end,
-    [3] = function() self.z = self.z - distance end,
+    [0] = function() self.z = self.z + distance end,
+    [1] = function() self.x = self.x + distance end,
+    [2] = function() self.z = self.z - distance end,
+    [3] = function() self.x = self.x - distance end,
     [4] = function() self.y = self.y + distance end,
     [5] = function() self.y = self.y - distance end
   }
 
   actions[orientation]()
+  self:saveState()
 end
 
 function WrappedTurtle:saveState()
@@ -318,6 +389,11 @@ function WrappedTurtle:saveState()
     x = self.x,
     y = self.y,
     z = self.z,
+    targetX = self.targetX,
+    targetY = self.targetY,
+    targetZ = self.targetZ,
+    targetOrientation = self.targetOrientation,
+    targetOrder = self.targetOrder,
     orientation = self.orientation,
     mode = self.mode,
   }
@@ -326,13 +402,23 @@ end
 
 function WrappedTurtle:loadState()
   local state = getState("turtle")
+  if state == nil then return end
   self.x = state.x
   self.y = state.y
   self.z = state.z
+  self.targetX = state.targetX
+  self.targetY = state.targetY
+  self.targetZ = state.targetZ
+  self.targetOrientation = state.targetOrientation
+  self.targetOrder = state.targetOrder
   self.orientation = state.orientation
   self.mode = state.mode
-  print("# Loaded state")
-  print(textutils.serialize(state))
+
+end
+
+function WrappedTurtle:setMode(mode)
+  self.mode = mode
+  self:saveState()
 end
 
 function WrappedTurtle:getOrientationSide(orientation)
@@ -351,18 +437,46 @@ function WrappedTurtle:getItemCount(slot)
 end
 
 function WrappedTurtle:depositInventory(slot1, slot2)
+  self:setMode("depositing inventory")
   slot1 = slot1 or 1
   slot2 = slot2 or 15
   for i = slot1, slot2 do
     self:drop(i, 64)
   end
+  self:setMode("nominal")
 end
 
-
 function WrappedTurtle:takeFuel(slot)
+  self:setMode("taking fuel")
   slot = slot or 16
-  local amount = 64 - self:getItemCount(16)
+  local amount = 64 - self:getItemCount(slot)
   self:suck(slot, amount)
+  self:setMode("nominal")
+  return self:getItemCount(slot) > 0
+end
+
+function WrappedTurtle:isInventoryOccupied(slot1, slot2)
+  slot1 = slot1 or 1
+  slot2 = slot2 or 15
+  
+  for i = slot1, slot2 do
+    if self:getItemCount(i) == 0 then
+      return false
+    end
+  end
+  return true
+end
+
+function WrappedTurtle:isInventoryFull(slot1, slot2)
+  slot1 = slot1 or 1
+  slot2 = slot2 or 15
+
+  for i = slot1, slot2 do
+    if self:getItemCount(i) ~= 64 then
+      return false
+    end
+  end
+  return true
 end
 
 return WrappedTurtle
--- programs/quarry.lua
+++ programs/quarry.lua
@@ -1,31 +1,76 @@
---$ARGS|Channel (40)|$ARGS
-
-local WrappedTurtle = require("/lua/lib/WrappedTurtle")
-
-local WT = WrappedTurtle:new(turtle)
+--$ARGS|Max X (10)|Max Z (10)|Max Y (10)|$ARGS
 
 -- Args
+local args = { ... }
+local MAX_X = tonumber(args[1]) or 3
+local MAX_Z = tonumber(args[2]) or 3
+local MAX_Y = tonumber(args[3]) or 3
+
+-- Libraries
+local stateHandler = require("/lua/lib/stateHandler")
+local WrappedTurtle = require("/lua/lib/WrappedTurtle")
+local WT = WrappedTurtle:new(turtle)
+
+-- Globals
+local X = 0
+local Z = 0
+local Y = 0
+local FIRST_TIME = true
+local ORES_FOUND = 0
 
 -- Main
 function start()
   print("# Program Started")
 
-  takeFuel()
-  WT:down(7, true)
-  WT:left(5, true)
-  WT:right(5, true)
-  WT:returnToTracker(true, { "x", "z", "y" })
+  -- Load and resume state if applicable
+  loadState()
+  WT:resume()
+
+  -- First time refuel attempt
+  if FIRST_TIME then takeFuel() end
+  FIRST_TIME = false
+  saveState()
+
+  WT:reorient()
+
+  -- Main loop
+  quarry()
+  
+  -- Final return and reposit
+  WT:returnToTracker(true, { "z", "x", "y" })
   depositInventory()
 
-  while true do sleep(10) end
+  local oresFound = ORES_FOUND
+  
+  -- Reset state
+  WT:resetTracker()
+  resetState()
 
+  print("# Quarry finished")
+  print("# Ores found " .. tostring(oresFound))
+  while true do sleep(100) end
+end
+
+function fetchAndDeposit()
+  print("# Fetching and Despositing")
+
+  WT:returnToTracker(true, { "z", "x", "y" })
+  takeFuel()
+  depositInventory()
+  WT:returnToTracker(true, { "y", "x", "z" })
+  WT:setMode("nominal")
 end
 
 function takeFuel()
   print("# Taking Fuel")
 
   WT:turnRight()
-  WT:refuel(true)
+  local attempts = 0
+  while not WT:takeFuel() do
+    attempts = attempts + 1
+    if attempts == 1 then print("No fuel in chest, will retry every 10 seconds...") end
+    sleep(10)
+  end
   WT:reorient()
 end
 
@@ -33,12 +78,150 @@ function depositInventory()
   print("# Depositing Inventory")
 
   WT:turnLeft()
+  local attempts = 0
   WT:depositInventory()
+  while WT:isInventoryOccupied() do
+    attempts = attempts + 1
+    if attempts == 1 then print("Can't deposit inventory, will retry every 10 seconds...") end
+    sleep(10)
+    WT:depositInventory()
+  end
   WT:reorient()
 end
 
-function printInfo()
-  print("orientation", WT.orientation, "x", WT.x, "y", WT.y, "z", WT.z)
+function inspectSurroundings()
+  -- forward
+  if checkBlock(WT:inspect()) then WT:dig() end
+  -- left
+  WT:turnLeft()
+  if checkBlock(WT:inspect()) then WT:dig() end
+  -- back
+  WT:turnLeft()
+  if checkBlock(WT:inspect()) then WT:dig() end
+  -- right
+  WT:turnLeft()
+  if checkBlock(WT:inspect()) then WT:dig() end
+  -- up and down
+  WT:turnLeft()
+  if checkBlock(WT:inspectUp()) then WT:digUp() end
+  if checkBlock(WT:inspectDown()) then WT:digDown() end
 end
+
+function checkBlock(success, block)
+  if not success then return false end
+  local tags = block["tags"]
+  if tags == nil then return false end
+  local isOre = tags["forge:ores"] == true
+  if isOre then ORES_FOUND = ORES_FOUND + 1 end
+  return isOre
+end
+
+function saveState()
+  local state = {
+    x = X,
+    y = Y,
+    z = Z,
+    firstTime = FIRST_TIME,
+    oresFound = ORES_FOUND
+  }
+  stateHandler.updateState("quarry", state)
+end
+
+function loadState()
+  local state = stateHandler.getState("quarry")
+  if state then
+    X = state.x
+    Z = state.z
+    Y = state.y
+    FIRST_TIME = state.firstTime
+    ORES_FOUND = state.oresFound
+    print("# Loaded Quarry State")
+    print(textutils.serialize(state))
+  end
+  return not not state
+end
+
+function resetState()
+  X = 0
+  Z = 0
+  Y = 0
+  FIRST_TIME = true
+  ORES_FOUND = 0
+  saveState()
+end
+
+function quarry()
+
+  function startY()
+
+    for y = Y, MAX_Y - 1 do
+      if math.fmod(Y, 2) == 1 then startX(Y) end
+      
+      while not WT:canDown(true) do fetchAndDeposit() end
+      Y = Y + 1
+      saveState()
+      WT:down(1, true)
+      inspectSurroundings()
+
+      if y == MAX_Y - 1 and math.fmod(Y, 2) == 1 then startX(Y) end
+    end
+
+  end
+
+  function startX(Y)
+    WT:turnLeft()
+
+    for x = X, MAX_X - 2 do
+      local _X = X
+      if Y % 4 == 1 then _X = _X + 1 end
+      if math.fmod(_X, 2) == 1 then startZ() end
+
+      while not WT:canForward(true) do fetchAndDeposit() end
+      X = X + 1
+      saveState()
+      WT:forward(1, true)
+      inspectSurroundings()
+
+      if x == MAX_X - 2 and math.fmod(_X + 1, 2) == 1 then startZ() end
+    end
+
+    WT:turnAround()
+
+    for x = 0, X - 1 do
+      while not WT:canForward(true) do fetchAndDeposit() end
+      X = X - 1
+      saveState()
+      WT:forward(1, true)
+    end
+
+    WT:turnLeft()
+  end
+
+  function startZ()
+    WT:turnRight()
+
+    for z = Z, MAX_Z - 2 do
+      while not WT:canForward(true) or WT:isInventoryOccupied() do fetchAndDeposit() end
+      Z = Z + 1
+      saveState()
+      WT:forward(1, true)
+      inspectSurroundings()
+    end
+
+    WT:turnAround()
+
+    for z = 0, Z - 1 do
+      while not WT:canForward() do fetchAndDeposit() end
+      Z = Z - 1
+      saveState()
+      WT:forward(1, true)
+    end
+
+    WT:turnRight()
+  end
+
+  startY()
+end
+
 
 start()
