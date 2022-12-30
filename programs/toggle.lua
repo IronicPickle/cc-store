b8a4ebee8826d4454a49c9cbf0af93ad02f0b850
IronicPickle <nathanrath@live.co.uk>
2022-12-30 18:58:43 +0000

    adjusted flicker randomness

--- programs/toggle.lua
+++ programs/toggle.lua
@@ -82,9 +82,9 @@ function on()
     if(flicker) then
         math.randomseed(os.time())
         for i = 1, 5, 1 do
+            rs.setAnalogOutput(redstoneOutput, i % 2 == 0 and 0 or 15)
             if(i % 2 == 0) then os.sleep(math.random(1, 3) / 2)
             else os.sleep(math.random()) end
-            rs.setAnalogOutput(redstoneOutput, i % 2 == 0 and 0 or 15)
         end
     else
         rs.setAnalogOutput(redstoneOutput, 15)
